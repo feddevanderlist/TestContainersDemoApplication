955b375e5e7805e574d4366f127a4d7d6041c3ae
vandalist <feddevanderlist@gmail.com>
2022-06-29 12:30:26 +0200

    use cassandra

--- src/main/java/nl/vandalist/repository/GebruikerRepository.java
+++ src/main/java/nl/vandalist/repository/GebruikerRepository.java
@@ -1,9 +1,9 @@
 package nl.vandalist.repository;
 
 import nl.vandalist.model.GebruikerDto;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GebruikerRepository extends JpaRepository<GebruikerDto, Long> {
+public interface GebruikerRepository extends CassandraRepository<GebruikerDto, Long> {
 }
