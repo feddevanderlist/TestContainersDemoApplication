ba88ed5cb1758b32c31a9384a3401f989cc8cb9a
vandalist <feddevanderlist@gmail.com>
2022-06-29 14:03:42 +0200

    add cassandra en repo

--- src/main/java/nl/vandalist/TestContainersDemoApplication.java
+++ src/main/java/nl/vandalist/TestContainersDemoApplication.java
@@ -2,8 +2,10 @@ package nl.vandalist;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @SpringBootApplication
+@EnableCassandraRepositories
 public class TestContainersDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestContainersDemoApplication.class, args);
--- src/main/java/nl/vandalist/repository/GebruikerRepository.java
+++ src/main/java/nl/vandalist/repository/GebruikerRepository.java
@@ -2,7 +2,8 @@ package nl.vandalist.repository;
 
 import nl.vandalist.model.GebruikerDto;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface GebruikerRepository extends CassandraRepository<GebruikerDto, Long> {
 }
